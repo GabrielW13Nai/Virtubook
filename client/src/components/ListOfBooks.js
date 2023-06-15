ec4001d4d6a455bc9ffc4a7ef4f22c6e20303f7c
Gabriel Wambugu <gabriel.wambugu@student.moringaschool.com>
2023-06-15 23:00:23 +0300

    Search done

--- client/src/components/ListOfBooks.js
+++ client/src/components/ListOfBooks.js
@@ -4,10 +4,10 @@ import { useEffect } from "react";
 import { Link } from 'react-router-dom';
 import BookRemove from './BookRemove';
 
-
 function ListOfBooks(){
     const [books, setBooks] = useState([])
-
+    const [filter, setFilter] = useState("")
+    const [filtered, setFiltered] = useState("")
 
     useEffect(()=>{
     fetch('/books')
@@ -17,15 +17,31 @@ function ListOfBooks(){
     });
     }, [])
 
+    useEffect(()=>{
+        const bookFilter = books.filter((book) =>{
+            return Object.values(book).join("").toLowerCase().includes(filter.toLowerCase())
+            })
+            setFiltered(bookFilter)
+    },[books, filter]);
 
     return (
     <>
+    <div className="search-ctn">
+        <div className="search-txtbx">
+            <input
+            className="textbox-text"
+            type="text"
+            placeholder="Search for book with keywords..."
+            onChange={e=> setFilter (e.target.value) }
+            />
+        </div>
+    </div>
     <Link to="/"><button className="Btn-book">Back to Dashboard</button></Link>
 
     <h1>Here are some books to choose from:</h1>
 
-    {books.map(book => (
-        <>
+    {filtered.length === 0?
+    books.map(book => (
     <section className='book-details'>
         <div className='bookimage'>
             <section className='image'>
@@ -48,14 +64,36 @@ function ListOfBooks(){
         </div>
         <BookRemove bookremove={books} setBookRemove={setBooks} book={book} />
     </section>
+    ))
+   : filtered.map(
+        (book => (
+            <section className='book-details'>
+                <div className='bookimage'>
+                    <section className='image'>
+                        <img
+                            src={book.image_url}
+                            alt='Not  found'
+                            width="400"
+                            height="450"
+                        />
+                    </section>
+                </div>
+                <div className='book-textstyle'>
+                 <label>Title: {book.title} </label> <br/>
+                 <label>Author: {book.author}</label> <br/>
+                 <label>Publisher:{book.publisher} </label> <br/>
+                 <label>Year Published: {book.year_published} </label> <br/>
+                 <label>Category: {book.category} </label>
+                </div>
+                <div>
+                </div>
+                <BookRemove bookremove={books} setBookRemove={setBooks} book={book} />
+            </section>)))}
     </>
-    ))}
+
+    )}
 
 
-    </>
-    )
-
-
-
-}
 export default ListOfBooks;
+
+
--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,53 +1,48 @@
-import React, { useState } from "react";
-import SearchList from "./SearchList";
-import Scroll from './Scroll';
+// import React, { useState } from "react";
+// import SearchList from "./SearchList";
+// import Scroll from './Scroll';
 
-function Search( {books} ){
-    const [searchBook, setSearchBook] = useState("");
-    const [show, setShow] = useState(false);
-
-    const filtered = books.filter(book => {
-        return (
-            book.title.toLowerCase().includes(searchBook.toLowerCase()) ||
-            book.author.toLowerCase().includes(searchBook.toLowerCase()) ||
-            book.publisher.toLowerCase().includes(searchBook.toLowerCase())
-            // book.year_published.includes(searchBook)
-        );
-    })
+// function Search( {books} ){
+//     const [searchBook, setSearchBook] = useState("");
+//     const [show, setShow] = useState(false);
 
 
-    function handleChange(e){
-        setSearchBook(e.target.value);
-        if(e.target.value===""){
-            setShow(false);}
-            else {
-                setShow(true)};
-        }
-    function ReturnSearch(){
-        if (show){
-        <Scroll>
-            <SearchList filtered={filtered} />
-        </Scroll>
-        }
 
-    }
+//     const searchParams = Object.keys(Object.assign({}, ...searchBook))
+//     const filtered = books.filter(book => {
+//             book.title.toString().toLowerCase().includes(searchBook.toLowerCase()) ||
+//             book.author.toString().toLowerCase().includes(searchBook.toLowerCase()) ||
+//             book.publisher.toString().toLowerCase().includes(searchBook.toLowerCase())
+//             // book.year_published.includes(searchBook)
+//     });
 
-    return(
-        <div className="search-ctn">
-            <div className="search-txtbx">
-                <label htmlFor="Search">Search</label>
-                    <input
-                    className="Search"
-                    type="text"
-                    placeholder="Search for book with keywords..."
-                    onChange={handleChange}/>
-            </div>
-            <div>
-                {ReturnSearch()}
-            </div>
 
-        </div>
-    )
-}
 
-export default Search
+
+//     function ReturnSearch(){
+//         if (show){
+//         <Scroll>
+//             <SearchList filtered={filtered} />
+//         </Scroll>
+//         }
+
+//     }
+
+//     return(
+//         <div className="search-ctn">
+//             <div className="search-txtbx">
+//                 <label htmlFor="Search">Search</label>
+//                     <input
+//                     className="textbox-text"
+//                     type="text"
+//                     placeholder="Search for book with keywords..."
+//                     onChange={handleChange}
+//                     />
+//             </div>
+//              {ReturnSearch()}
+
+//         </div>
+//     )
+// }
+
+// export default Search
